b5d2590fd28161fee292065465854ac0da18c831
Faldine <ecenam0202gbo@gmail.com>
2025-04-11 15:02:55 +0100

    mise à jour

--- src/api/services/task.services.js
+++ src/api/services/task.services.js
@@ -1,14 +1,51 @@
-const TaskModel = require('../models/task.models');
-// Afficher tous les articles
-const getAllTask = async () => {
-    await TaskModel.find()
+const TaskModel = require("../models/task.models");
+
+
+const getAllTask = async ()=> {
+    const task = await TaskModel.find();
+    return task;
+};
+const getTaskByEmail = async (email)=>{
+    const task = await TaskModel.findOne({email});
+    return task;
+};
+const getTaskById = async (id) => {
+    const task = await TaskModel.findById(id);
+}
+
+const createTask= async(taskData)=>{
+    const tasksExist = await getTaskByEmail(taskData.email);
+    if (tasksExist) {
+        throw new Error ("Email already exists");
+    }
+    const task = await TaskModel.create(taskData);
+    return task;
+};
+const updateTask = async (id, taskData)=>{
+const taskExist = await getTaskById(id);
+if(!taskExist) {
+    throw new Error ("task not found");
+}
+const task = await TaskModel.findByIdAndUpdate(id, taskData, {
+    new:true,
+});
+return task;
+
+};
+const deleteTask = async (id) => {
+    const taskExist = await getTaskById(id);
+    if(!taskExist) {
+        throw new Error ("Task not found");
+    }
+    await TaskModel.findByIdAndDelete(id);
+    return "Task deleted successfully";
 };
 
-// create article
-const createTask = async (data) => {
-    const newTask = await TaskModel.create(data);
-    return newTask;
-};
-
-
-module.exports = {getAllTask, createTask }
+module.exports = {
+    getAllTask,
+    getTaskByEmail,
+    getTaskById,
+    createTask,
+    updateTask,
+    deleteTask,
+}
